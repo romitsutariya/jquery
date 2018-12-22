ff6ada72d27d2f20e200e85d522e4e81c59301f3
romitsutariya <=romitsutariya@gmail.com>
2018-12-22 12:39:32 +0530

    added whtsapp.py file

---
+++ whatsapp.py
@@ -0,0 +1,12 @@
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By 
+import time 
+
+# Replace below path with the absolute path 
+# to chromedriver in your computer 
+browser = webdriver.Firefox('/home/kalubha/Downloads/python/geckoDriver') 
+browser.get('https://www.twitter.com') 
+  
